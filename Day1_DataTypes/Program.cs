c1fdf0684e6001b21591f419634ab065e3a059d7
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-08 13:57:22 +0300

    Add project files.

---
+++ Day1_DataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day1_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+            // Declare second integer, double, and String variables.
+            int myInt;
+            double myDouble;
+            string myString;
+            // Read and save an integer, double, and String to your variables.
+            myInt = Convert.ToInt32(Console.ReadLine());
+            myDouble = Convert.ToDouble(Console.ReadLine());
+            myString = Console.ReadLine();
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i + myInt);
+            double sum = myDouble + d;
+            Console.WriteLine(String.Format("{0:0.0}", sum));
+            Console.WriteLine(s + myString);
+
+        }
+    }
+}
---
+++ HackerRank30DaysOfCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
