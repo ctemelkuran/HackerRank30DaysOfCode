502ac97af2b7a1a7c7a0c02a58554a59d0767f24
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-10 17:04:25 +0300

    Day3_IntroToConditional

---
+++ Day3_IntroToConditional/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day3_IntroToConditional
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            
+            if ()
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N%2==0 && N >= 2 && N <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (true)
+            {
+
+            }
+        }
+        public bool IsEven(int n)
+        {
+            if (n%2 == 0)
+            {
+                return true;
+                
+            }
+            
+            return false;
+        }
+    }
+}
