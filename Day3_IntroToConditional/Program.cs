484c88c8918749f0a7410da13205776e8dbba1ae
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-10 17:14:26 +0300

    summary>

--- Day3_IntroToConditional/Program.cs
+++ Day3_IntroToConditional/Program.cs
@@ -4,12 +4,12 @@ namespace Day3_IntroToConditional
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-
+            //Read N
             int N = Convert.ToInt32(Console.ReadLine());
-            
+
             if (!IsEven(N))
             {
                 Console.WriteLine("Weird");
@@ -17,29 +17,24 @@ namespace Day3_IntroToConditional
             else if (IsEven(N))
             {
                 if (N >= 2 && N <= 5)
-                {
                     Console.WriteLine("Not Weird");
-                }
                 else if (N >= 6 && N <= 20)
-                {
                     Console.WriteLine("Weird");
-                }
-                else if (N > 20)
-                {
-                    Console.WriteLine("Not Weird");
-                }
-
+                else if (N > 20)                
+                    Console.WriteLine("Not Weird");                
             }
-           
         }
+        /// <summary>
+        /// Check if the given parameter is even
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
         static bool IsEven(int n)
         {
             if (n % 2 == 0)
             {
                 return true;
-
             }
-
             return false;
         }
     }
