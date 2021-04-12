d6ba2478b56ae0a3f9ef8d0383fa26c1c4125bfe
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-12 11:33:40 +0300

    for lop

---
+++ Day5_Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day5_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = i * n;
+                Console.WriteLine("{0} x {1} = {2}", n, i, result);
+            }
+        }
+    }
+}
