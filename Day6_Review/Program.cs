1d4c63808642877b5396c5fdfd4d7d36d704d427
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-13 19:50:28 +0300

    int T

---
+++ Day6_Review/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day6_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string S with a length of N
+            // print its even-indexed and odd-indexed characters as space-separated strings on a single line
+            //take input T for test cases
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                string S = Console.ReadLine();
+            }
+        }
+    }
+}
