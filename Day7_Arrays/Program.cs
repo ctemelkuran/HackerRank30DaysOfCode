e648a250ae191d5ba92b38fca86d50396b510d81
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-16 16:29:30 +0300

    Day7_Arrays

---
+++ Day7_Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Day7_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            
+            for (int i = n; i >= 0; i--)
+            {
+                Array reverse = Array.Reverse(arr);
+                Console.Write("{0} ", arr[i]);
+            }
+            
+
+        }
+    }
+}
