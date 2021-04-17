2c9b16d2cecde3efd53e158c66f416bee0d1e5d6
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-17 16:30:00 +0300

    dictionaries

--- Day6_Review/Program.cs
+++ Day6_Review/Program.cs
@@ -16,22 +16,70 @@ namespace Day6_Review
             for (int i = 0; i < T; i++)
             {
                 stringList.Add(Console.ReadLine());
-                
+
             }
-            
+
             foreach (var item in stringList)
             {
-                
-                for (int i = 0; i < item.Length; i+=2)
+
+                for (int i = 0; i < item.Length; i += 2)
                 {
                     Console.Write(item[i]);
                 }
                 Console.Write(" ");
-                for (int i = 1; i < item.Length; i+=2)
+                for (int i = 1; i < item.Length; i += 2)
                 {
                     Console.Write(item[i]);
                 }
                 Console.WriteLine("");
+            }
+
+            //ArrayQueries();
+
+        }
+
+        private static void ArrayQueries()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[] A = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int Q = Convert.ToInt32(Console.ReadLine());
+
+
+            int[][] LR = new int[Q][];
+
+
+            for (int i = 0; i < Q; i++)
+            {
+                LR[i] = Array.ConvertAll(Console.ReadLine().Split(' '), LRTemp => Convert.ToInt32(LRTemp));
+            }
+            int sml = 0;
+            int sml2nd = 9999;
+            int j = 0;
+            for (int a = 0; a < Q; a++)
+            {
+                for (int i = LR[a][0]; i < LR[a][1]; i++)
+                {
+                    if (sml > A[i])
+                    {
+                        sml = A[i];
+                        j = i;
+                    }
+
+                }
+            }
+            for (int a = 0; a < Q; a++)
+            {
+                for (int i = LR[a][0]; i < LR[a][1]; i++)
+                {
+                    if (sml > A[i])
+                    {
+                        sml = A[i];
+                        j = i;
+                    }
+
+                }
             }
         }
     }
---
+++ Day8_DictionariesAndMaps/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace Day8_DictionariesAndMaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                string input = Console.ReadLine();
+                string[] items = input.Split();
+                phoneBook[items[0]] = items[1];
+                if (items.Length > 2)
+                {
+                    Console.WriteLine("2 values must be entered");
+                }
+                else if (items[1].Length >8)
+                {
+                    Console.WriteLine("8 values must be entered");
+                }
+                
+                  
+                
+                
+            }
+            List<string> names = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+                //string[] namesnput  = Console.ReadLine().ToString(); 
+                
+            }
+
+            
+                foreach (var name in names)
+                {
+                
+                    if (phoneBook.ContainsKey(name))
+                    {
+                        Console.WriteLine("{0}={1}", name, phoneBook[name]);
+                    }
+                    else if (!phoneBook.ContainsKey(name))
+                    {
+                        Console.WriteLine("Not found");
+                    }
+                
+            }
+        }
+    }
+}
