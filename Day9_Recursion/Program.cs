9de26240cc5d316c93192d492dacf23f3ffca6a3
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-18 15:08:58 +0300

    factorial

---
+++ Day9_Recursion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Day9_Recursion
+{
+    class Program
+    {
+
+        // Complete the factorial function below.
+        static int factorial(int n)
+        {
+            
+            if (n >= 1)
+            {
+                return n * factorial(n - 1);
+            }
+            else
+            {
+                return 1;
+            }
+                
+            
+           
+        }
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            Console.WriteLine(result);
+
+        }
+    }
+
+}
