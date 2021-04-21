34359bd76d49a5f2cc39861778572fa0c2584ee9
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-21 11:04:35 +0300

    max is found

--- Day11_2DArrays/Program.cs
+++ Day11_2DArrays/Program.cs
@@ -7,22 +7,28 @@ namespace Day11_2DArrays
         static void Main(string[] args)
         {
             int[][] arr = new int[6][];
-            int sum = 0, max = 0;
+            int sum = 0, max = -64;
 
             for (int i = 0; i < 6; i++)
             {
                 arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
             }
 
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 1; i < 5; i++)
             {
-                for (int j = 0; j < (arr[i].Length-2); j++)
+                for (int j = 1; j < (arr[i].Length-1); j++)
                 {
-                    sum = arr[i][j];
-                    //Console.Write(arr[i][j] + " ");
+                    sum = arr[i][j] + arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1]
+                        + arr[i+1][j-1]+ arr[i+1][j] + arr[i+1][j+1];
+                    if (max <= sum)
+                    {
+                        max = sum;
+                    }
+                    
                 }
-                //Console.WriteLine();
+               
             }
+            Console.WriteLine(max);
             Console.ReadLine();
         }
     }
