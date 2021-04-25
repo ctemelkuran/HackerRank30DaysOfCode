d3c83edd2a7a25fe056ab766683e26719edd3344
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-25 20:38:23 +0300

    Math.Abs(elements.Max()-elements.Min());

---
+++ Day14_Scope/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Day14_Scope
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        // Add your code here
+        public Difference(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void computeDifference()
+        {
+            //for (int i = 0; i < elements.Length; i++)
+            //{
+            //    maximumDifference = Math.Abs(elements[i] )
+            //}
+            maximumDifference = Math.Abs(elements.Max()-elements.Min());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}
