de9a6236107fd83895a93249bd68737c0ff7fe8f
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-27 14:39:27 +0300

    insert method

--- Day15_LinkedList/Program.cs
+++ Day15_LinkedList/Program.cs
@@ -15,10 +15,22 @@ namespace Day15_LinkedList
 	}
 	class Program
 	{
-
+		//head is pointer
 		public static Node insert(Node head, int data)
 		{
 			//Complete this method
+			Node newNode = new Node(data);
+            if (head==null)
+            {
+				return newNode;
+            }
+			Node current = head;
+            while (current.next != null)
+            {
+				current = current.next;
+            }
+			current.next = newNode;
+			return head;
 		}
 
 		public static void display(Node head)
