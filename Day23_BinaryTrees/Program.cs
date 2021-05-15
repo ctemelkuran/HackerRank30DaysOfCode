1f636729ce1097850ded255c7518f234ce00bc2b
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-05-15 14:25:30 +0300

    day23

---
+++ Day23_BinaryTrees/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day23_BinaryTrees
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+    class Program
+    {
+        
+
+        static void levelOrder(Node root)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            if (root != null)
+            {
+                queue.Enqueue(root.data);
+
+                
+            }
+
+
+            Console.WriteLine("{0} ",);
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            levelOrder(root);
+        }
+    }
+}
