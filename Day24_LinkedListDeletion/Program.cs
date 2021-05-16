1d93f811bb7b21b8a61252f0af9e2172e4f47269
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-05-16 16:47:21 +0300

    removed lines

--- Day24_LinkedListDeletion/Program.cs
+++ Day24_LinkedListDeletion/Program.cs
@@ -12,72 +12,68 @@ namespace Day24_LinkedListDeletion
             data = d;
             next = null;
         }
-		
+
     }
     class Program
     {
         public static Node removeDuplicates(Node head)
         {
-			
-			
-			Node prev = head;
-			while (prev != null)
-			{
+            Node prev = head;
+
+            while (prev != null)
+            {
                 if (prev.next != null && prev.data == prev.next.data)
                 {
-					// Remove node from Linked List
-					prev.next = prev.next.next;
+                    // Remove node from Linked List
+                    prev.next = prev.next.next;
 
-				}
-				else
-					prev = prev.next;
-			}
+                }
+                else
+                    prev = prev.next;
+            }
+            return head;
+        }
 
-			
-
-			return head;
-		}
-
-		public static Node insert(Node head, int data)
-		{
-			Node p = new Node(data);
+        public static Node insert(Node head, int data)
+        {
+            Node p = new Node(data);
 
 
-			if (head == null)
-				head = p;
-			else if (head.next == null)
-				head.next = p;
-			else
-			{
-				Node start = head;
-				while (start.next != null)
-					start = start.next;
-				start.next = p;
+            if (head == null)
+                head = p;
+            else if (head.next == null)
+                head.next = p;
+            else
+            {
+                Node start = head;
+                while (start.next != null)
+                    start = start.next;
+                start.next = p;
 
-			}
-			return head;
-		}
-		public static void display(Node head)
-		{
-			Node start = head;
-			while (start != null)
-			{
-				Console.Write(start.data + " ");
-				start = start.next;
-			}
-		}
-		static void Main(String[] args)
-		{
+            }
+            return head;
+        }
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+        static void Main(String[] args)
+        {
 
-			Node head = null;
-			int T = Int32.Parse(Console.ReadLine());
-			while (T-- > 0)
-			{
-				int data = Int32.Parse(Console.ReadLine());
-				head = insert(head, data);
-			}
-			head = removeDuplicates(head);
-			display(head);
-		}
-	}
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            head = removeDuplicates(head);
+            display(head);
+        }
+    }
 }
