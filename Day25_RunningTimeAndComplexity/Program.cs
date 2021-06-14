14b6f54941c987dea999bcdcccb263d3d3986dc8
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-06-14 13:52:47 +0300

    list.Add(n);

--- Day25_RunningTimeAndComplexity/Program.cs
+++ Day25_RunningTimeAndComplexity/Program.cs
@@ -9,14 +9,19 @@ namespace Day25_RunningTimeAndComplexity
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
             int T = Int32.Parse(Console.ReadLine());
             while (T-- > 0)
             {
                 int n = Convert.ToInt32(Console.ReadLine());
-                bool isPrime = true;
-                for (int i = 2; i < n; i++)
+                list.Add(n);
+            }
+            bool isPrime = true;
+            foreach (var item in list)
+            {
+                for (int i = 2; i < item; i++)
                 {
-                    if (n % i == 0)
+                    if (item % i == 0)
                     {
                         isPrime = false;
                     }
@@ -30,6 +35,7 @@ namespace Day25_RunningTimeAndComplexity
                     Console.WriteLine("Not prime");
                 }
             }
+            
         }
     }
 }
